ecd9d7b00c80a970abb9ad2b76b421ea06bab414
ksm <ksmles36@gmail.com>
2023-04-04 19:36:45 +0900

    Initial commit

---
+++ src/main/java/com/ksm/jpashop/JpAshopApplication.java
@@ -0,0 +1,13 @@
+package com.ksm.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksm/jpashop/JpAshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
